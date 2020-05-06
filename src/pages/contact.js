95c5944bfcd19186b51bd0d598454a932b09ab9f
mattfrca <42378602+mattfrca@users.noreply.github.com>
2020-05-06 15:16:33 +0200

    Contact page added - no css

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,35 +1,27 @@
 import React from 'react'
-import { graphql } from 'gatsby'
-import { HelmetDatoCms } from 'gatsby-source-datocms'
 import Layout from "../components/layout"
 
-const Contact = ({data: { contact }}) => {
+const Contact = () => {
   return (
   <Layout>
-    {/* <article className="sheet legals">
-      <HelmetDatoCms seo={legal.seoMetaTags} />
-      <div className="sheet__inner">
-        <div
-          className="sheet__body"
-          dangerouslySetInnerHTML={{
-            __html: legal.ContactNode.childMarkdownRemark.html,
-          }}
-        />
-      </div>
-    </article> */}
+    <form className="contact-form" data-netlify="true" name="formulaire-contact" method="POST">
+        <input type="text" name="name"/>
+        <label htmlFor="name">Nom & Prénom</label>
+        <input type="text" name="ville"/>
+        <label htmlFor="ville">Ville</label>
+        <input type="text" name="rue"/>
+        <label htmlFor="rue">Rue</label>
+        <input type="text" name="rue"/>
+        <label htmlFor="rue">Rue</label>
+        <input type="text" name="postal"/>
+        <label htmlFor="postal">Code Postal</label>
+        <input type="mail" name="mail"/>
+        <label htmlFor="mail">Courriel</label>
+        <input type="text" name="rue"/>
+        <label htmlFor="rue">Rue</label>
+        <button type="submit">Envoyer</button>
+    </form>
   </Layout>
 )}
 
 export default Contact
-
-export const query = graphql`
-  query contactQuery {
-    contact: datoCmsMentionsLegale {
-      mentionsLegalesNode {
-        childMarkdownRemark {
-          html
-        }
-      }
-    }
-  }
-`
