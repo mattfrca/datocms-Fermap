b11c63850ba2f58fe7ae2365feed2af880513bdf
mattfrca <42378602+mattfrca@users.noreply.github.com>
2020-05-06 16:26:39 +0200

    contact style added - netlify connected

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,22 +4,33 @@ import Layout from "../components/layout"
 const Contact = () => {
   return (
   <Layout>
-    <form className="contact-form" data-netlify="true" name="formulaire-contact" method="POST">
-        <input type="text" name="name"/>
-        <label htmlFor="name">Nom & Prénom</label>
-        <input type="text" name="ville"/>
-        <label htmlFor="ville">Ville</label>
-        <input type="text" name="rue"/>
-        <label htmlFor="rue">Rue</label>
-        <input type="text" name="rue"/>
-        <label htmlFor="rue">Rue</label>
-        <input type="text" name="postal"/>
-        <label htmlFor="postal">Code Postal</label>
-        <input type="mail" name="mail"/>
-        <label htmlFor="mail">Courriel</label>
-        <input type="text" name="rue"/>
-        <label htmlFor="rue">Rue</label>
-        <button type="submit">Envoyer</button>
+    <form className="contact-form" name="formulaire-contact" method="POST" data-netlify-honeypot="bot-field" data-netlify="true">
+        <p>
+            <label>Nom complet <span>*</span><input type="text" name="name" /></label>   
+        </p>
+        <p>
+            <label>Adresse mail <span>*</span><input type="email" name="email" /></label>
+        </p>
+        <p>
+            <label>Ville <span>*</span><input type="text" name="ville" /></label>
+        </p>
+        <p>
+            <label>Adresse <span>*</span><input type="text" name="adresse" /></label>
+        </p>
+        <p>
+            <label>Code postal <span>*</span><input type="text" name="code_postal" /></label>
+        </p>
+        <p>
+            <label>Message<textarea name="message" /></label>
+        </p>
+        <p>
+            <label>Utilisation des données <span>*</span><input id="utilisation-donnees" type="checkbox" value="En soumettant ce formulaire, j’accepte que les informations saisies soient exploitées
+             dans le cadre de la demande de contact et de la relation commerciale qui peut en découler."></input>
+             <label id="description-utilisation-donnees">En soumettant ce formulaire, j’accepte que les informations saisies soient exploitées
+             dans le cadre de la demande de contact et de la relation commerciale qui peut en découler.</label></label>
+        </p>
+        <input id="send-button" value="Envoyer" type="submit" className="contact-form-btn"/>
+        <p><span>*</span> : Champs obligatoires</p>
     </form>
   </Layout>
 )}
